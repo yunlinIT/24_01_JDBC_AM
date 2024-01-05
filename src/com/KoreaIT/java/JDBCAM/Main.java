db93c1aacf5a20f51eebf310cae971f7d9217c8b
yunlinIT <yunlinit@gmail.com>
2024-01-05 16:40:16 +0900

    게시물 작성시 DB에 INSERT

--- src/com/KoreaIT/java/JDBCAM/Main.java
+++ src/com/KoreaIT/java/JDBCAM/Main.java
@@ -1,5 +1,9 @@
 package com.KoreaIT.java.JDBCAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -28,13 +32,62 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
-
+				
 				Article article = new Article(id, title, body);
 				articles.add(article);
 
 				lastArticleId = id;
 
 				System.out.println(id + "번 글이 등록되었습니다");
+				
+				//////////////////////
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBC_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article ";
+					sql += "SET regDate = NOW(),";
+					sql += "updateDate = NOW(),";
+					sql += "title = '" + title + "',";
+					sql += "`body`= '" + body + "';";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					int affectedRow = pstmt.executeUpdate();
+
+					System.out.println("affectedRow : " + affectedRow);
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+				
 			} else if (cmd.equals("article list")) {
 				System.out.println("==목록==");
 				if (articles.size() == 0) {
