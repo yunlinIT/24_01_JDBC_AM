bbd7d1eb10378f79aa0958063b8ed63285ddf71b
yunlinIT <yunlinit@gmail.com>
2024-01-05 15:59:27 +0900

    JDBC Insert 테스트

--- DB.sql
+++ DB.sql
@@ -1,3 +1,23 @@
 DROP DATABASE IF EXISTS `JDBC_AM`;
 CREATE DATABASE `JDBC_AM`;
 USE `JDBC_AM`;
+
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+SHOW TABLES;
+
+SELECT *
+FROM article;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목', RAND()),
+`body` = CONCAT('내용', RAND());
