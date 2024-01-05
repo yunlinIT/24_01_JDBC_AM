25e11e291923d9654ac924e302fe2b348ac840b5
yunlinIT <yunlinit@gmail.com>
2024-01-05 17:55:14 +0900

    JDBC Insert 테스트

--- src/com/KoreaIT/java/JDBCAM/Article.java
+++ src/com/KoreaIT/java/JDBCAM/Article.java
@@ -3,6 +3,8 @@ package com.KoreaIT.java.JDBCAM;
 public class Article {
 
 	private int id;
+	private String regDate;
+	private String updateDate;
 	private String title;
 	private String body;
 
@@ -12,9 +14,34 @@ public class Article {
 		this.body = body;
 	}
 
+	public Article(int id, String regDate, String updateDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+	}
+
 	@Override
 	public String toString() {
-		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
 	}
 
 	public int getId() {
