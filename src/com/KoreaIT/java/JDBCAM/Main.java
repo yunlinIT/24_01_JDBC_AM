c5b490141afb94b8779eefeb39ae1f4d8be60078
yunlinIT <yunlinit@gmail.com>
2024-01-08 09:59:38 +0900

    게시물 리스팅 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/java/JDBCAM/Main.java
+++ src/com/KoreaIT/java/JDBCAM/Main.java
@@ -3,6 +3,7 @@ package com.KoreaIT.java.JDBCAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +13,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-
-		List<Article> articles = new ArrayList<>();
 
 		int lastArticleId = 0;
 
@@ -32,15 +31,10 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
-				
-				Article article = new Article(id, title, body);
-				articles.add(article);
 
 				lastArticleId = id;
 
 				System.out.println(id + "번 글이 등록되었습니다");
-				
-				//////////////////////
 
 				Connection conn = null;
 				PreparedStatement pstmt = null;
@@ -87,9 +81,79 @@ public class Main {
 					}
 				}
 
-				
 			} else if (cmd.equals("article list")) {
 				System.out.println("==목록==");
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+
+				List<Article> articles = new ArrayList<>();
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBC_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC;";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					rs = pstmt.executeQuery(sql);
+
+					while (rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						Article article = new Article(id, regDate, updateDate, title, body);
+
+						articles.add(article);
+
+					}
+//					for (int i = 0; i < articles.size(); i++) {
+//						System.out.println("번호 : " + articles.get(i).getId());
+//						System.out.println("등록 날짜 : " + articles.get(i).getRegDate());
+//						System.out.println("수정 날짜 : " + articles.get(i).getUpdateDate());
+//						System.out.println("제목 : " + articles.get(i).getTitle());
+//						System.out.println("내용 : " + articles.get(i).getBody());
+//					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 					continue;
